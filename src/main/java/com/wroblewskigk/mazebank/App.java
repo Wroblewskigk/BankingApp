4f4d3dfa0ecbaa6344884505c759cc9df2edb647
wroblewskigk <wroblewskigk@proton.me>
2025-02-02 15:42:12 +0100

    Implement login screen UI

--- src/main/java/com/wroblewskigk/mazebank/App.java
+++ src/main/java/com/wroblewskigk/mazebank/App.java
@@ -1,7 +1,16 @@
 package com.wroblewskigk.mazebank;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Maze Bank");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
