0c91163dd3f56eb605f7e27b8a3d6d6fc41cadea
wroblewskigk <wroblewskigk@proton.me>
2025-02-08 20:37:35 +0100

    Add factory methods to ViewFactory.java

--- src/main/java/com/wroblewskigk/mazebank/Views/ViewFactory.java
+++ src/main/java/com/wroblewskigk/mazebank/Views/ViewFactory.java
@@ -1,4 +1,51 @@
 package com.wroblewskigk.mazebank.Views;
 
+import com.wroblewskigk.mazebank.Controllers.Client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ViewFactory {
+    // Client Views
+    private AnchorPane dashboardView;
+
+    public ViewFactory() {}
+
+    public AnchorPane getDashboardView() {
+        // Prevents multiple loads of the interface when the user changes between menus
+        if (dashboardView == null) {
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Bank");
+        stage.show();
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Client.fxml"));
+        ClientController clientController = new ClientController();
+        loader.setController(clientController);
+        createStage(loader);
+    }
 }
